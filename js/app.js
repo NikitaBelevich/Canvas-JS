b76be7f00c7f73d30193aadb46eb1f55be51e5ce
Nikita Belevich <belevish_1998@mail.ru>
2020-08-08 11:08:21 +0300

    Фигуры

--- js/app.js
+++ js/app.js
@@ -5,6 +5,11 @@ let canvas = document.querySelector('.canvas_1');
 // Получили контекст этого canvas
 let ctx = canvas.getContext('2d');
 
+const PI = Math.PI;
+function getRadians(degrees) {
+	return (Math.PI/180)*degrees;
+}
+
 ctx.beginPath();
 //1
 ctx.moveTo(10, 10);
@@ -43,3 +48,88 @@ ctx.fillRect(70, 70, 50, 50);
 ctx.clearRect(80, 80, 20, 20);
 ctx.closePath();
 ctx.stroke();
+
+// Круги, дуги
+ctx.beginPath();
+ctx.arc(60, 200, 40, 0, getRadians(360));
+ctx.stroke();
+ctx.closePath();
+
+
+ctx.beginPath();
+ctx.arc(160, 200, 40, 0, getRadians(360));
+ctx.closePath();
+ctx.stroke();
+ctx.fill();
+ctx.closePath();
+
+
+ctx.beginPath();
+ctx.arc(260, 200, 40, 0, getRadians(180));
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.arc(360, 200, 40, 0, getRadians(180));
+ctx.closePath();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.arc(460, 200, 40, 0, getRadians(180));
+ctx.closePath();
+ctx.fill();
+
+ctx.beginPath();
+ctx.moveTo(20, 400);
+ctx.quadraticCurveTo(140, 350, 80, 300);
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.moveTo(120, 400);
+ctx.bezierCurveTo(180, 400, 180, 300, 120, 300);
+ctx.stroke();
+ctx.closePath();
+
+// Градиенты
+let grd1 = ctx.createLinearGradient(0, 0, 150, 0);
+grd1.addColorStop(0, '#000');
+grd1.addColorStop(0.5, '#fff');
+grd1.addColorStop(0.6, 'red');
+grd1.addColorStop(1, 'lime');
+
+ctx.beginPath();
+ctx.fillStyle = grd1;
+ctx.rect(10, 430, 150, 100);
+ctx.fill();
+ctx.closePath();
+
+// Не работает радиальный градиент по какой-то причине
+let radgrd1 = ctx.createRadialGradient(75, 50, 5, 90, 60, 50);
+radgrd1.addColorStop(0, 'blue');
+radgrd1.addColorStop(0.5, '#fff');
+
+ctx.beginPath();
+ctx.fillStyle = radgrd1;
+ctx.rect(200, 430, 150, 100);
+ctx.fill();
+ctx.closePath();
+
+
+let img1 = new Image();
+img1.src = './img/img_lamp.jpg';
+img1.onload = () => {
+    let pattr1 = ctx.createPattern(img1, 'repeat');
+    ctx.fillStyle = pattr1;
+    ctx.fillRect(400, 430, 150, 100);
+};
+
+ctx.font = '20px sans-serif';
+ctx.fillStyle = '#000';
+ctx.fillText('Hello World!', 10, 600);
+
+// ctx.rotate(getRadians(90));
+
+ctx.strokeRect(50, 610, 10, 10);
+ctx.scale(2,2);
+ctx.strokeRect(50, 610, 10, 10);
